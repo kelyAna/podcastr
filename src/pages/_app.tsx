8a01ca30d5b497f53a358517476a424091c35f0b
Ana Kely Lopes <kely@alu.ufc.br>
2021-04-20 16:16:08 -0300

    Add Component Player implementation

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,17 @@
 import { Header } from '../components/Header/index'
+import { Player } from '../components/Player'
 
+import styles from '../styles/app.module.scss'
 import '../styles/global.scss'
 
 function MyApp({ Component, pageProps }) {
-  return(
-    <div>
-      <Header/>
+  return (
+    <div className={styles.appWrapper}>
+      <main>
+        <Header />
+        <Component {...pageProps} />
+      </main>
+      <Player/>
     </div>
   )
 }
