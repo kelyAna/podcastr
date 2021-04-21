7375613769fa61ae3e823c27fa3ea914f413a261
Ana Kely Lopes <kely@alu.ufc.br>
2021-04-21 13:42:42 -0300

    Add episodes list

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,31 @@
+import { GetStaticProps } from 'next'
 
-export default function Home() {
+type HomeProps = {
+  episodes: Array<{
+    id: String;
+    titulo: String;
+    members: String;
+  }>
+}
+
+export default function Home(props: HomeProps) {
   return (
-   <div></div>
+   <div>
+     <h1>Index</h1>
+     <p>{JSON.stringify(props.episodes)}</p>
+   </div>
   )
 
+}
+export const getStaticProps: GetStaticProps = async () => {
+  const response = await fetch('http://localhost:3333/episodes')
+  const data = await response.json()
+
+
+  return {
+    props: {
+      episodes: data,
+    },
+    revalidate: 60 * 60 * 8,
+  }
 }
