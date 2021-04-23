226149df624a0155d408925f0acb18c59fd906de
Ana Kely Lopes <kely@alu.ufc.br>
2021-04-22 21:06:42 -0300

    Add funcion refs

--- src/components/Player/index.tsx
+++ src/components/Player/index.tsx
@@ -1,5 +1,5 @@
 import Image from 'next/image'
-import { useContext } from 'react'
+import { useContext, useRef, useEffect } from 'react'
 import Slider from 'rc-slider'
 
 import 'rc-slider/assets/index.css'
@@ -9,7 +9,26 @@ import { PlayerContext } from '../../contexts/PlayerContext'
 import styles from '../Player/styles.module.scss'
 
 export function Player() {
-  const { episodeList, currentEpisodeIndex } = useContext(PlayerContext)
+  const audioRef = useRef<HTMLAudioElement>(null)
+
+  const { 
+    episodeList, 
+    currentEpisodeIndex, 
+    isPlaying, 
+    togglePlay 
+  } = useContext(PlayerContext)
+
+  useEffect(() => {
+    if(!audioRef.current){
+      return;
+    }
+
+    if(isPlaying){
+      audioRef.current.play()
+    }else{
+      audioRef.current.pause()
+    }
+  }, [isPlaying])
 
   const episode = episodeList[currentEpisodeIndex]
 
@@ -58,6 +77,7 @@ export function Player() {
         { episode && (
           <audio 
             src={episode.url}
+            ref={audioRef}
             autoPlay
            />
 
@@ -73,8 +93,13 @@ export function Player() {
             <img src="/play-previous.svg" alt="Tocar anterior" />
           </button>
 
-          <button type="button" disabled={!episode}>
-            <img src="/play.svg" alt="Tocar" />
+          <button type="button" disabled={!episode} onClick={togglePlay}>
+            { isPlaying ? (
+              <img src="/pause.svg" alt="Pause" />
+
+            ) : (
+              <img src="/play.svg" alt="Tocar" />
+            )}
           </button>
 
           <button type="button" disabled={!episode}>
--- src/contexts/PlayerContext.ts
+++ src/contexts/PlayerContext.ts
@@ -11,7 +11,9 @@ type Episode = {
 type PlayerContextData = {
   episodeList: Episode[];
   currentEpisodeIndex: number;
-  play: (episode: Episode) => void
+  isPlaying: boolean;
+  play: (episode: Episode) => void;
+  togglePlay: () => void;
 }
 
 export const PlayerContext = createContext({} as PlayerContextData)
